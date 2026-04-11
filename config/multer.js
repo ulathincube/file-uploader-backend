285a085307cadc9159dd447ec2a01adbe2cc6b52
Ulathi Ncube <lathincube@gmail.com>
2026-04-11 05:15:15 +0200

    Fix: remove uploads folder from tracking

--- config/multer.js
+++ config/multer.js
@@ -1,26 +1,9 @@
 import multer from 'multer'
 import { extname } from 'node:path'
 
-const createStorage = (location) =>
-  multer.diskStorage({
-    destination: (req, file, callback) => {
-      if (req.folderName) {
-        return callback(null, `temp/${req.folderName}`)
-      }
-      callback(null, 'temp/')
-    },
-    filename: (req, file, callback) => {
-      const savedName = `${file.fieldname}-${Date.now()}${extname(file.originalname)}`
-      callback(null, savedName)
-    },
-  })
-
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
-    if (req.folderName) {
-      return callback(null, `temp/${req.folderName}`)
-    }
-    callback(null, 'temp/')
+    callback(null, 'uploads/')
   },
   filename: (req, file, callback) => {
     const savedName = `${file.fieldname}-${Date.now()}${extname(file.originalname)}`
@@ -34,17 +17,5 @@ const upload = multer({
     fileSize: 1000000,
   },
 }).single('file')
-
-// const callUpload = (location) => {
-//   const storage = multer.diskStorage({
-//     destination: (req, file, callback) => {
-//       return callback(null, `temp/${location}`)
-//     },
-//     filename: (req, file, callback) => {
-//       const savedName = `${file.fieldname}-${Date.now()}${extname(file.originalname)}`
-//       callback(null, savedName)
-//     },
-//   })
-// }
 
 export default upload
